fe297ecc80ed8d97608bcf01153133cb88c05c19
plavelo <plastic.velocity@gmail.com>
2021-01-11 12:56:04 +0900

    added some tests for seq & skip APIs

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,48 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(Debug, Clone, PartialEq)]
+enum Value {
+    None,
+    Some(String),
+    List(Vec<Value>),
+}
+
 #[derive(Debug)]
 struct Success {
-    pub position: i32,
-    pub value: String,
+    pub position: usize,
+    pub value: Value,
 }
 
 #[derive(Debug)]
 struct Failure {
-    pub position: i32,
+    pub position: usize,
     pub expected: Vec<String>,
 }
 
 trait Reply {
-    fn position(&self) -> i32;
-    fn err_position(&self) -> i32;
-    fn value(&self) -> String;
+    fn position(&self) -> usize;
+    fn err_position(&self) -> usize;
+    fn value(&self) -> Value;
     fn expected(&self) -> Vec<String>;
 }
 
 impl Reply for Result<Success, Failure> {
-    fn position(&self) -> i32 {
+    fn position(&self) -> usize {
         match self {
             Ok(success) => success.position,
             Err(failure) => failure.position,
         }
     }
 
-    fn err_position(&self) -> i32 {
+    fn err_position(&self) -> usize {
         match self {
-            Ok(_) => -1,
+            Ok(_) => 0,
             Err(failure) => failure.position,
         }
     }
 
-    fn value(&self) -> String {
+    fn value(&self) -> Value {
         match self {
             Ok(success) => success.value.clone(),
             Err(_) => panic!(),
@@ -48,7 +55,7 @@ impl Reply for Result<Success, Failure> {
     fn expected(&self) -> Vec<String> {
         match self {
             Ok(_) => panic!(),
-            Err(failure) => failure.expected.clone(),
+            Err(failure) => failure.expected.to_vec(),
         }
     }
 }
@@ -57,6 +64,9 @@ fn merge_results(
     curr: Result<Success, Failure>,
     last: Result<Success, Failure>,
 ) -> Result<Success, Failure> {
+    if last.position() == -1 {
+        return curr;
+    }
     if curr.is_ok() {
         return curr;
     }
@@ -74,9 +84,9 @@ fn merge_results(
     return last;
 }
 
-type Parser = dyn Fn(String, i32) -> Result<Success, Failure>;
+type Parser = dyn Fn(&str, i32) -> Result<Success, Failure>;
 
-fn parse(parser: Box<Parser>, source: String) -> Result<Success, Failure> {
+fn parse(parser: Box<Parser>, source: &str) -> Result<Success, Failure> {
     skip(parser, eof())(source, 0)
 }
 
@@ -90,7 +100,7 @@ fn eof() -> Box<Parser> {
         } else {
             Ok(Success {
                 position: position,
-                value: "".to_string(),
+                value: Value::None,
             })
         }
     });
@@ -105,13 +115,13 @@ fn string(string: &'static str) -> Box<Parser> {
                 expected: vec![string.to_string()],
             });
         }
-        let head = string.get(position as usize..to as usize);
+        let head = source.get(position as usize..to as usize);
         return match head {
             Some(s) => {
                 if s == string {
                     Ok(Success {
                         position: to,
-                        value: s.to_string(),
+                        value: Value::Some(s.to_string()),
                     })
                 } else {
                     Err(Failure {
@@ -129,20 +139,33 @@ fn string(string: &'static str) -> Box<Parser> {
 }
 
 fn skip(curr: Box<Parser>, next: Box<Parser>) -> Box<Parser> {
-    map(seq(vec![curr, next]), Box::new(|value: String| value))
+    map(
+        seq(vec![curr, next]),
+        Box::new(|value: Value| match value {
+            Value::List(val) => match val.first() {
+                Some(v) => match v {
+                    Value::None => Value::None,
+                    Value::Some(string) => Value::Some(string.clone()),
+                    Value::List(list) => Value::List(list.to_vec()),
+                },
+                None => Value::None,
+            },
+            _ => panic!(),
+        }),
+    )
 }
 
 fn seq(parsers: Vec<Box<Parser>>) -> Box<Parser> {
     return Box::new(move |source, position| -> Result<Success, Failure> {
-        let mut reply: Result<Success, Failure> = Err(Failure {
-            position: 0,
-            expected: Vec::new(),
+        let mut reply: Result<Success, Failure> = Ok(Success {
+            position: -1,
+            value: Value::None,
         });
         let mut pos = position;
-        let mut acc: Vec<String> = Vec::new();
+        let mut acc: Vec<Value> = Vec::new();
         let src = Rc::new(source);
         for parser in &parsers {
-            reply = merge_results(parser(src.clone().to_string(), pos), reply);
+            reply = merge_results(parser(&src, pos), reply);
             if reply.is_err() {
                 return reply;
             }
@@ -152,14 +175,14 @@ fn seq(parsers: Vec<Box<Parser>>) -> Box<Parser> {
         return merge_results(
             Ok(Success {
                 position: pos,
-                value: acc.join(""),
+                value: Value::List(acc),
             }),
             reply,
         );
     });
 }
 
-fn map(parser: Box<Parser>, func: Box<dyn Fn(String) -> String>) -> Box<Parser> {
+fn map(parser: Box<Parser>, func: Box<dyn Fn(Value) -> Value>) -> Box<Parser> {
     return Box::new(move |src, pos| -> Result<Success, Failure> {
         let reply = parser(src, pos);
         if reply.is_ok() {
@@ -183,14 +206,29 @@ mod tests {
     #[test]
     fn string_ok() {
         let parser = string("source");
-        let result = parse(parser, "source".to_string());
+        let result = parse(parser, "source");
         assert_eq!(result.is_ok(), true);
     }
 
     #[test]
     fn string_error() {
         let parser = string("source");
-        let result = parse(parser, "other".to_string());
+        let result = parse(parser, "other");
         assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn skip_ok() {
+        let parser = skip(string("x"), string("y"));
+        let result = parse(parser, "xy");
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().value, Value::Some("x".to_string()));
+    }
+
+    #[test]
+    fn seq_ok() {
+        let parser = seq(vec![string("key"), string(":"), string("value")]);
+        let result = parse(parser, "key:value");
+        assert_eq!(result.is_ok(), true);
     }
 }
